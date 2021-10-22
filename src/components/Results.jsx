2464e58221dcd3054ea185494badb7a4a5d21008
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-10-22 18:38:14 +0200

    feat: Add Search.jsx to components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -25,27 +25,27 @@ const Results = () => {
     if (loading) return <Loading />;
 
     switch (location.pathname) {
-        case '/search':
-            return (
-                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-                    {results?.map(({ link, title, description }, index) => (
-                        <div key={index} className="w-full md:w-2/5">
-                            {console.log(results.results)}
-                            <a href={link} target="_blank" rel="noreferrer">
-                                <p className="text-md dark:text-white-500 ">
-                                    {link.lenght > 10 ? link.substring(0, 10) : link}
-                                </p>
-                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
-                                    {title}
-                                </p>
-                                <p className="text-sm hover:underline dark:text-gray-300 text-gray-700">
-                                    {description}
-                                </p>
-                            </a>
-                        </div>
-                    ))}
-                </div>
-            );
+        // case '/search':
+        //     return (
+        //         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+        //             {results?.map(({ link, title, description }, index) => (
+        //                 <div key={index} className="w-full md:w-2/5">
+        //                     {console.log(results.results)}
+        //                     <a href={link} target="_blank" rel="noreferrer">
+        //                         <p className="text-md dark:text-white-500 ">
+        //                             {link.lenght > 10 ? link.substring(0, 10) : link}
+        //                         </p>
+        //                         <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+        //                             {title}
+        //                         </p>
+        //                         <p className="text-sm hover:underline dark:text-gray-300 text-gray-700">
+        //                             {description}
+        //                         </p>
+        //                     </a>
+        //                 </div>
+        //             ))}
+        //         </div>
+        //     );
         case '/images':
             return (
                 <div className="flex flex-wrap justify-center items-center">
